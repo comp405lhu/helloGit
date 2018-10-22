6a2692409fd9d58c0542d0b47676433051f73731
Krish Pillai <kpillai@ld-1173.lhup.edu>
2018-10-22 11:59:23 -0400

    Added a new method

--- HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
+++ HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
@@ -2,6 +2,9 @@ package edu.lockhaven.comp405;
 
 public class HelloGit {
 
+	public static void myMethod() {
+		System.out.println("Hello myMethod()");
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello again");
