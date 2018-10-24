e84b1a82c7060ecbda6bcefba6ff378a3d49f9cd
Assunta DeSanto <assuntad23@gmail.com>
2018-10-24 11:57:31 -0400

    assunta's edit

--- HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
+++ HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
@@ -4,12 +4,12 @@ public class HelloGit {
 
 	public static void myMethod() {
 		System.out.println("Hello myMethod()");
+		System.out.println("The number "+ 23);
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello again");
 		System.out.println("Another Test");
-		System.out.println("Krish: branch1");
+		System.out.println("Assunta: assuntaBranch > Assunta is always first!");
 	}
-
 }
