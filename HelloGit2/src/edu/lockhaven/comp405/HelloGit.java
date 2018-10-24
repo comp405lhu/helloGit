c41e3d6e993481609d492479a17423a7ad4ae90c
Shaun S Donohue <sxd8141@ld-1185.lhup.edu>
2018-10-24 11:35:24 -0400

    Changes by Shaun!

--- HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
+++ HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
@@ -1,12 +1,18 @@
 package edu.lockhaven.comp405;
 
+
+//$Author$ Shaun Donohue
+//I'm the best!
 public class HelloGit {
 
+	public static void myMethod() {
+		System.out.println("Hello myMethod()");
+
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello again");
 		System.out.println("Another Test");
-		System.out.println("Poop");
 	}
 
 }
