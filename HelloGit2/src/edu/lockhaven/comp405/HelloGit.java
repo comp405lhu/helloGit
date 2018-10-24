ef1bcb77b7aa6e044aad8c2d79c43d0b52d24b75
assuntad23 <assuntad23@gmail.com>
2018-10-24 12:13:08 -0400

    added dogVotes method

--- HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
+++ HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
@@ -10,6 +10,15 @@ public class HelloGit {
 		System.out.println("Hello myMethod()");
 		System.out.println("The number "+ 23);
 	}
+	
+	public static int dogVotes() {
+		int dogVotes = 0;
+		for (int i = 0; i < dogVotes; i ++) {
+			dogVotes *=0;
+		}
+		return dogVotes;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello again");
