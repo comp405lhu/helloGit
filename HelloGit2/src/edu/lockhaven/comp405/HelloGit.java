47e644bb83c983f9a4f6c8f3b25b13db67a8cbb1
Glenn Valerio <grv5783@localhost>
2018-10-24 12:17:08 -0400

    Glenn's Pizza

--- HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
+++ HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
@@ -2,14 +2,18 @@ package edu.lockhaven.comp405;
 
 public class HelloGit {
 
-	public static void myMethod() {
-		System.out.println("Hello myMethod()");
+	public static void Pizza() {
+		System.out.println("Pizza");
 	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello again");
 		System.out.println("Another Test");
 		System.out.println("Krish: branch1");
+		System.out.println("Time");
+		System.out.println("Dogs are involved with comuniyt service, the workforce, and the military as well.");
+		System.out.println("They have every right to vote!");
 	}
 
 }
