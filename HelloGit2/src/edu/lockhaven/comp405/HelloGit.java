c679279f25c895766f44f8935aa000b2edd9b5c7
Shaun S Donohue <sxd8141@ld-1185.lhup.edu>
2018-10-24 12:11:15 -0400

    Added Shaun method!

--- HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
+++ HelloGit2/src/edu/lockhaven/comp405/HelloGit.java
@@ -25,12 +25,18 @@ public class HelloGit {
 	public static void myMethod() {
 		System.out.println("Hello myMethod()");
 	}
+	
+	public static void shaun()
+	{
+		System.out.println("Shaun");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello again");
 		System.out.println("Another Test");
 		System.out.println("Krish: branch1");
+		shaun();
 	}
 
 }
